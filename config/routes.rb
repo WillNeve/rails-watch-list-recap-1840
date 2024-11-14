109f846fe27426de2eb848aa00dcb55cd41bcded
William Neve <williamneve6000@gmail.com>
2024-11-14 20:10:46 +0000

    post livecode

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'bookmarks/create'
   root to: 'lists#index'
 
-  resources :lists, only: [:index, :show] do
+  resources :lists, only: [:show] do
     resources  :bookmarks, only: [:create]
   end
 
@@ -10,6 +9,4 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
